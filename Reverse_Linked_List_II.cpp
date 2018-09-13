bf6f85aa928b07553298fa9c1d813268b6ab00bc
lhuang <lhuang9703@gmail.com>
2018-09-13 19:07:07 +0800

    Create Reverse_Linked_List_II.cpp

---
+++ Reverse_Linked_List_II.cpp
@@ -0,0 +1,64 @@
+// Source : https://leetcode-cn.com/problems/reverse-linked-list-ii/description/
+// Number : 92
+// Author : HL
+// Date   : 2018-09-13
+// Kill   : 96.40%
+
+/********************************************************************************** 
+Reverse a linked list from position m to n. Do it in one-pass.
+
+Note: 1 ≤ m ≤ n ≤ length of list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL, m = 2, n = 4
+Output: 1->4->3->2->5->NULL
+**********************************************************************************/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        if (head == NULL)
+            return head;
+        
+        ListNode *p = head, *q = head, *r = head->next, *s;
+        int step = 0;
+        while (r != NULL) {
+            if (step < m-1) {
+                p = p->next;
+                q = q->next;
+                r = r->next;
+            } else if (step == n-1) {
+                break;
+            } else {
+                s = r->next;
+                r->next = q;
+                q = r;
+                r = s;
+            }
+            step++;
+        }
+        
+        s = head;
+        if (s == p) {
+            p->next = r;
+            head = q;
+        } else {
+            while (s->next != p) {
+                s = s->next;
+            }
+            s->next = q;
+            p->next = r;
+        }
+        
+        return head;
+    }
+};
