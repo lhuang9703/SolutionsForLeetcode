d166ca929873a25e79841c97ec346d816dba74ee
lhuang <lhuang9703@gmail.com>
2018-10-12 00:05:18 +0800

    Update Path_Sum_II.cpp

--- Path_Sum_II.cpp
+++ Path_Sum_II.cpp
@@ -1,4 +1,3 @@
-
 // Source : https://leetcode-cn.com/problems/path-sum-ii/description/
 // Number : 113
 // Author : HL
@@ -7,13 +6,10 @@
 
 /********************************************************************************** 
 Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
-
 Note: A leaf is a node with no children.
 
 Example:
-
 Given the below binary tree and sum = 22,
-
       5
      / \
     4   8
@@ -21,8 +17,8 @@ Given the below binary tree and sum = 22,
   11  13  4
  /  \    / \
 7    2  5   1
-Return:
 
+Return:
 [
    [5,4,11,2],
    [5,8,4,5]
