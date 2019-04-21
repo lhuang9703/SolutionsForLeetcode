993af4b1b54397cc9f3fa37e2ca15731f1bd560b
lhuang <lhuang9703@gmail.com>
2019-04-21 14:57:23 +0800

    Update Remove_Linked_List_Elements.cpp

--- Remove_Linked_List_Elements.cpp
+++ Remove_Linked_List_Elements.cpp
@@ -24,6 +24,34 @@ Output: 1->2->3->4->5
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
+        if (head == NULL)
+            return NULL;
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* p = dummy;
+        while (p->next != NULL)
+        {
+            if (p->next->val == val)
+                p->next = p->next->next;
+            else
+                p = p->next;
+        }
+        return dummy->next;
+    }
+};
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
         ListNode *p = head;
         ListNode *q;
         while(p != NULL){
